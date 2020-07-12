c0ae0d3bbbe67482d0728e871cd14a6670caea98
Olivier OUEMBA <olivier.ouemba@hotmail.com>
2020-07-12 23:36:25 +0200

    gradle wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,8 @@ val signingKey: String? = project.findProperty("signingKey") as String?
     ?: System.getenv("SIGNING_KEY")?.replace("\\n", System.lineSeparator())
 val signingPassword: String? = project.findProperty("signingPassword") as String? ?: System.getenv("SIGNING_PASSWORD")
 val repoUrl = "https://github.com/elieof/eoo"
+println(signingPassword)
+println(signingKey)
 subprojects {
 
     repositories {
