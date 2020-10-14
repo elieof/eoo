975863137d421b6aba5bf0011291434e58d45539
Olivier O <draker.ko@yandex.com>
2020-10-14 16:14:04 +0000

    EOO-T-1:  :construction_worker:  Try publishing in build pipeline

--- .space.kts
+++ .space.kts
@@ -5,5 +5,17 @@
 */
 
 job("Build and run tests") {
-   gradlew("openjdk:11", "build")
+   container("openjdk:11") {
+        env["USERNAME"] = Params("github_username")
+        env["PASSWORD"] = Secrets("github_token")
+        env["MAVEN_USERNAME"] = Params("ossrh_username")
+        env["MAVEN_PASSWORD"] = Secrets("ossrh_password")
+        env["SIGNING_KEY"] = Params("ossrh_signing_key_1") + Params("ossrh_signing_key_2") + Params("ossrh_signing_key_3") +
+       		Params("ossrh_signing_key_4") + Params("ossrh_signing_key_5") + Params("ossrh_signing_key_6") +
+       		Params("ossrh_signing_key_7") + Params("ossrh_signing_key_8")
+        env["SIGNING_PASSWORD"] = Secrets("ossrh_signing_password")
+       kotlinScript { api ->
+           api.gradlew("build publish")
+       }
+   }
 }
