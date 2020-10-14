dc26743f8047140fb111e46b94c560eaf47ae222
Olivier O <draker.ko@yandex.com>
2020-10-14 16:31:57 +0000

    EOO-T-1:  :construction_worker: Try publishing in build pipeline 2

--- .space.kts
+++ .space.kts
@@ -10,12 +10,21 @@ job("Build and run tests") {
         env["PASSWORD"] = Secrets("github_token")
         env["MAVEN_USERNAME"] = Params("ossrh_username")
         env["MAVEN_PASSWORD"] = Secrets("ossrh_password")
-        env["SIGNING_KEY"] = Params("ossrh_signing_key_1") + Params("ossrh_signing_key_2") + Params("ossrh_signing_key_3") +
-       		Params("ossrh_signing_key_4") + Params("ossrh_signing_key_5") + Params("ossrh_signing_key_6") +
-       		Params("ossrh_signing_key_7") + Params("ossrh_signing_key_8")
+        env["SIGNING_KEY_1"] = Params("ossrh_signing_key_1")    
+        env["SIGNING_KEY_2"] = Params("ossrh_signing_key_2")
+        env["SIGNING_KEY_3"] = Params("ossrh_signing_key_3")    
+        env["SIGNING_KEY_4"] = Params("ossrh_signing_key_4")
+        env["SIGNING_KEY_5"] = Params("ossrh_signing_key_5")    
+        env["SIGNING_KEY_6"] = Params("ossrh_signing_key_6")
+        env["SIGNING_KEY_7"] = Params("ossrh_signing_key_7")    
+        env["SIGNING_KEY_8"] = Params("ossrh_signing_key_8")
         env["SIGNING_PASSWORD"] = Secrets("ossrh_signing_password")
        kotlinScript { api ->
-           api.gradlew("build publish")
+           var signingKey = System.getenv("SIGNING_KEY_1")
+           (2..8).forEach {
+               signingKey += System.getenv("SIGNING_KEY_$it")
+           }
+           api.gradlew("build publish -PsigningKey=\"$signingKey\"")
        }
    }
 }
