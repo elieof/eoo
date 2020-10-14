ffa9f717672faafad5ca442daf98ec331a79b03e
Olivier O <draker.ko@yandex.com>
2020-10-14 16:33:26 +0000

    EOO-T-1:  :construction_worker: Fix Secrets keyword in pipeline

--- .space.kts
+++ .space.kts
@@ -6,18 +6,18 @@
 
 job("Build and run tests") {
    container("openjdk:11") {
-        env["USERNAME"] = Params("github_username")
+        env["USERNAME"] = Secrets("github_username")
         env["PASSWORD"] = Secrets("github_token")
-        env["MAVEN_USERNAME"] = Params("ossrh_username")
+        env["MAVEN_USERNAME"] = Secrets("ossrh_username")
         env["MAVEN_PASSWORD"] = Secrets("ossrh_password")
-        env["SIGNING_KEY_1"] = Params("ossrh_signing_key_1")    
-        env["SIGNING_KEY_2"] = Params("ossrh_signing_key_2")
-        env["SIGNING_KEY_3"] = Params("ossrh_signing_key_3")    
-        env["SIGNING_KEY_4"] = Params("ossrh_signing_key_4")
-        env["SIGNING_KEY_5"] = Params("ossrh_signing_key_5")    
-        env["SIGNING_KEY_6"] = Params("ossrh_signing_key_6")
-        env["SIGNING_KEY_7"] = Params("ossrh_signing_key_7")    
-        env["SIGNING_KEY_8"] = Params("ossrh_signing_key_8")
+        env["SIGNING_KEY_1"] = Secrets("ossrh_signing_key_1")    
+        env["SIGNING_KEY_2"] = Secrets("ossrh_signing_key_2")
+        env["SIGNING_KEY_3"] = Secrets("ossrh_signing_key_3")    
+        env["SIGNING_KEY_4"] = Secrets("ossrh_signing_key_4")
+        env["SIGNING_KEY_5"] = Secrets("ossrh_signing_key_5")    
+        env["SIGNING_KEY_6"] = Secrets("ossrh_signing_key_6")
+        env["SIGNING_KEY_7"] = Secrets("ossrh_signing_key_7")    
+        env["SIGNING_KEY_8"] = Secrets("ossrh_signing_key_8")
         env["SIGNING_PASSWORD"] = Secrets("ossrh_signing_password")
        kotlinScript { api ->
            var signingKey = System.getenv("SIGNING_KEY_1")
